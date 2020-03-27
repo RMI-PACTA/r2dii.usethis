ab3537490e716eaece3326cdd59e19ef5770d36d
Mauro Lepore <maurolepore@gmail.com>
2020-03-27 12:49:16 -0500

    New use_r2dii_data() (#15)
    * New use_r2dii_data()
    
    * Accept name of new dataset as argument
    
    * Remove unused features
    
    * Update readme

---
+++ tests/testthat/test-use_r2dii_data.R
@@ -0,0 +1,9 @@
+test_that("outputs an informative checklist", {
+  verify_output(
+    test_path("output", "output-use_r2dii_data.txt"), {
+      
+    use_r2dii_data()
+    
+    use_r2dii_data("my_data")
+  })
+})
